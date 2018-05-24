e50b9cbaee5688b9023d01ff706111b814c30374
Sander <sander0527@gmail.com>
2018-05-24 15:18:26 +0200

    Send command

--- src/BPPArduino.java
+++ src/BPPArduino.java
@@ -20,9 +20,18 @@ public class BPPArduino extends Arduino {
 
         if(commandNumber == 5) {
             // hier de logica bepalen welke doos het voorwerp inmoet.
-
-
-            this.writeCurrentPort("54");
+            int boxNumber=0;
+            Product nextProduct =products.get(0);
+            for(Storage box:boxes){
+                for(Product product:box.getProducts()){
+                    if(nextProduct==product){
+                        boxNumber=box.getBoxNumber();
+                    }
+                }
+            }
+            String command="5";
+            command+=boxNumber;
+            this.writeCurrentPort(command);
         }
     }
 
@@ -34,7 +43,6 @@ public class BPPArduino extends Arduino {
         if(products == null) {
             popUp.error("Er zijn geen producten geinitialiseerd");
         }
-
         sendCommand(5);
 
     }
